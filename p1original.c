6f9e51c8e929f1c34f8c4f12c67ade93ff20977e
Ridha Salam <96277953+ridhasalam786@users.noreply.github.com>
2022-02-05 17:28:37 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
 
+void input(int *a,int *b)
+{
+  printf ("Enter two numbers: \t");
+  scanf ("%d %d",a,b);
+}
+
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+
+void output(int a,int b,int sum)
+{
+  printf ("Sum of %d+%d is %d",a,b,sum);
+}
+
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(x,y,z);
+  return 0;
 }

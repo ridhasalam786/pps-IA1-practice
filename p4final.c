c2dca66a3e752b4aa4995bd7a8d59bbe420b7506
Ridha Salam <96277953+ridhasalam786@users.noreply.github.com>
2022-02-06 05:57:19 +0000

    p4original

---
+++ p4final.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int input_array_size()
+{
+  int n;
+  printf ("Enter the array size: \t");
+  scanf ("%d",&n);
+  return n;
+}
+
+void input_array(int n,int a[n])
+{
+  printf ("Enter the array elements: \n");
+  for (int i=0;i<n;i++)
+  scanf ("%d",&a[i]);
+}
+
+int sum_n_arrays(int n,int a[n])
+{
+  int sum=0;
+  for (int i=0;i<n;i++)
+  sum=sum+a[i];
+
+  return sum;
+}
+
+void output(int n,int a[n],int sum)
+{
+  for (int i=0;i<n-1;i++)
+  printf ("%d + ",a[i]);
+
+  printf ("%d is %d ",a[n-1],sum);
+}
+
+int main()
+{
+  int x,y;
+  x=input_array_size();
+  int a[x];
+  input_array(x,a);
+  y=sum_n_arrays(x,a);
+  output(x,a,y);
+  return 0;
+}
---
+++ p4original.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int input_array_size()
+{
+  int n;
+  printf ("Enter the value of n: \t");
+  scanf ("%d",&n);
+  return n;
+}
+
+void input_array(int n,int a[n])
+{
+  printf ("Enter the array elements: ");
+  for (int i=0;i<n;i++)
+  scanf ("%d",&a[i]);
+}
+
+int sum_n_arrays(int n,int a[n])
+{
+  int sum=0;
+  for (int i=0;i<n;i++)
+  sum+=a[i];
+  return sum;
+}
+
+void output(int n,int a[n],int sum)
+{
+  for (int i=0;i<n;i++)
+  printf ("Array elements:%d \t",a[i]);
+
+  for(int i=0;i<n-1;i++)
+  printf ("%d+",a[i]);
+
+  printf ("%d is %d",a[n-1],sum);
+}
+
+int main()
+{
+  int x,y;
+  x=input_array_size();
+  int a[x];
+  input_array(x,&a[x]);
+  y=sum_n_arrays(x,&a[x]);
+  output(x,&a[x],y);
+  return 0;
+}

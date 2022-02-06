b9d15799ff4f2e16307381f8800f76571567ff42
Ridha Salam <96277953+ridhasalam786@users.noreply.github.com>
2022-02-06 05:00:29 +0000

    p3original.c

---
+++ p3original.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int input_n()
+{
+  int n;
+  printf ("Enter the value of n: \t");
+  scanf ("%d",&n);
+  return n;
+}
+
+int sum_n(int n)
+{
+  int sum=0;
+  for(int i=1;i<=n;i++)
+  sum+=i;
+  return sum;
+}
+
+void output(int n,int sum)
+{
+  for(int i=1;i<n;i++)
+  printf ("%d+",i);
+
+  printf ("%d is %d",n,sum);
+}
+
+int main()
+{
+  int x,y;
+  x=input_n();
+  y=sum_n(x);
+  output(x,y);
+  return 0;
+}
